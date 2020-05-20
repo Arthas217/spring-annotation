924a72d45cbae2282769972244916670e5795cc3
zuocheng <442854399@qq.com>
2020-05-20 18:08:50 +0800

    bean

--- src/main/java/org/jixi/config/ConfigClass.java
+++ src/main/java/org/jixi/config/ConfigClass.java
@@ -14,7 +14,6 @@ public class ConfigClass {
     // 注册组件 （相当于原来xml中 bean标签）
     // spring容器注入一个组件Bean  使用注解@Bean
     // 类型class是返回类型  id默认是用方法名person01  也可以用value指定获取bean的名字
-
     @Bean(value = "person")
     public Person person01() {
         return new Person("zlk", 19);
