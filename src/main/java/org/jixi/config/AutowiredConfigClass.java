130180fc2da529eaf58c2549ff81d0a4b652efd1
zuocheng <442854399@qq.com>
2020-06-29 11:46:55 +0800

    自动装配

--- src/main/java/org/jixi/config/AutowiredConfigClass.java
+++ src/main/java/org/jixi/config/AutowiredConfigClass.java
@@ -4,6 +4,7 @@ import org.jixi.dao.BookDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * 自动装配：spring利用依赖注入（DI),完成对IOC容器中各个组件的依赖关系赋值
@@ -15,6 +16,7 @@ public class AutowiredConfigClass {
     // 注册同类型BookDao组件 目的BookService中通过自动注入存在一个同类型的BookDao组件
     // 类型BookDao  id =默认是方法名，也可以在 @Bean(value = "dd")指定bean的名字
     // 断点发现 this指代的是代理类对象
+    @Primary
     @Bean
 //    @Bean(value = "bookDao")
     public BookDao bookDao1() {
--- src/main/java/org/jixi/service/BookService.java
+++ src/main/java/org/jixi/service/BookService.java
@@ -11,15 +11,19 @@ import org.springframework.stereotype.Service;
  * 1. @Autowired自动注入，把组件BookDao拿过来，如果找到就赋值。
  * 2. 默认优先按照类型去容器中找对应的组件，类似application.getBean(BookDao.class);
  * 3. 如果在容器中找到多个同类型的bean组件，那再按照BookService类中属性的名字作为组件的id去容器中查找;类似application.getBean("bookDao");
+ * 4. @Qualifier("bookDao1") 指定需要装配的组件的id，而不是使用属性名
+ * 5. 自动装配时，默认一定要让BookDao类（组件）的属性赋值好，即去掉@Repository和@Bean方式--无BookDao组件），否则报错。
+ *    如果避免报错，可以使用@Autowired(required = false) 这时属性bookDao值为null
+ * 6. @Primary,让spring进行自动装配时，默认使用首选的bean，如果有@Qualifier注解的话，优先选择指定的@Qualifier
  */
 @Service
 public class BookService {
 
-    //指定需要装配的组件的id，而不是使用属性名
-    @Qualifier("bookDao1")
-    @Autowired
-//    private BookDao bookDao;
+    @Qualifier("bookDao")
+//    @Autowired
+    @Autowired(required = false)
     private BookDao bookDao;
+//    private BookDao bookDao1;
 
     public void printBookDao(){
         System.out.println(bookDao);
